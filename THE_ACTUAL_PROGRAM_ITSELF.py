a494855cdbe902a86ff659e6748a96fda3608222
Ray Louis <x1337c@gmail.com>
2022-02-25 09:50:28 -0800

    starting work today

--- THE_ACTUAL_PROGRAM_ITSELF.py
+++ THE_ACTUAL_PROGRAM_ITSELF.py
@@ -1,8 +1,20 @@
-def printHi():
-  print('hi')
+def step1():
+    pass
+
+
+def step2():
+    pass
+
+
+def step3():
+    pass
+
 
 def main():
-    printHi()
+    print("ECS 129 Protein Structure Comparison Program")
+    step1()
+    step2()
+    step3()
 
-if __name__ == "__main__":
-   main()
+
+main()
