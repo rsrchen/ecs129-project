fc46ed895d78fc7e026538779e909438e7338936
Ray Louis <x1337c@gmail.com>
2022-03-02 20:23:20 -0800

    yes

--- Protein Structure Prediction Program.py
+++ Protein Structure Prediction Program.py
@@ -7,8 +7,20 @@ print("Argument List:", str(sys.argv))
 so. how's it gonna be?
 
 default settings:
-ingest one sequence only. 
+ingest one sequence only
+ask for 4 character PDB ID code, else crash
+directory containing PDB file is ./pdb_files
 directory containing coordinate files (seq1rank1 thru seq1goldstandard) is ./coordinate_files
 
+changeable settings:
+# of sequences to do rmsd shit for
+PDB files directory
+coords txt files directory
+
+steps:
+1. generate pdb file of only alpha carbons, nicely formatted, spaces where they ought to be
+2. generate alpha carbon coords plaintext file from that ^ and put it in coordinate files (goodbye awk. it'll be too hard for me to reach into the WSL via python. i'll have to code this myself.)
+3. read that coordinate file and do the rmsd shit!
+4. repeat for all the sequences the user inputs
 
 """
---
+++ modules/awk_but_not_really.py
@@ -0,0 +1,4 @@
+
+def awk_kinda():
+    with open() as pdb_file:
+      pass
