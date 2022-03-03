e8e2eb34e5643342220ee58c2d1880f5b98691c4
Ray Louis <x1337c@gmail.com>
2022-03-03 00:14:23 -0800

    last commit here. i'm bout to go hard in the paint. no more relying on reading files excessively. here we go.

--- modules/call_parser_and_awk.py
+++ modules/call_parser_and_awk.py
@@ -1,10 +1,9 @@
 from pathlib import Path
-import asyncio
 import awk_but_not_really
 import parse_pdb
 
 
-async def execute():
+def execute():
     a = "pdb_files"
     p = Path("./")
     alphafold_prediction_pdb_files = p.glob(a + "/*.pdb")
@@ -13,11 +12,6 @@ async def execute():
         for x in alphafold_prediction_pdb_files
     ]
     
-    [
-        awk_but_not_really.create_coordinates_text_file(x, "1ab1")
-        for x in alphafold_prediction_pdb_files
-    ]
-    # return name_of_clean_well_spaced_pdb_file
 
 
-asyncio.run(execute())
+execute()
--- modules/parse_pdb.py
+++ modules/parse_pdb.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+from unicodedata import name
 from Bio.PDB import PDBParser
 from Bio.PDB import PDBIO
+
 # from Bio.SeqUtils import seq1
 from Bio.PDB.PDBIO import Select
 
@@ -28,11 +30,16 @@ class AlphaCarbonSelect(Select):
 
 def get_alpha_carbons(filename: str | Path, pdb_id: str):
     pdb_parser = PDBParser()
-    pdb_io = PDBIO()
+    # pdb_io = PDBIO()
     protein = pdb_parser.get_structure(id=pdb_id, file=filename)
-    pdb_io.set_structure(protein)
-    pdb_io.save((str(filename).split(".pdb")[0]) + "_alpha_carbons.pdb", AlphaCarbonSelect())
-    pass
+    protein_atoms = protein.get_atoms()
+    alpha_carbon_coords = []
+    [
+        alpha_carbon_coords.append(atom.coord)
+        for atom in protein_atoms
+        if atom.name == "CA"
+    ]
+    return alpha_carbon_coords
+    # pdb_io.set_structure(protein)
 
-
-pass
+    # pdb_io.save((str(filename).split(".pdb")[0]) + "_alpha_carbons.pdb", AlphaCarbonSelect())
