94dd440f4475bbd18c6f89f2500011df1068ff06
Ray Louis <x1337c@gmail.com>
2022-03-05 14:48:30 -0800

    added a sequence length calculator

--- modules/parse_pdb.py
+++ modules/parse_pdb.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from unicodedata import name
 from Bio.PDB import PDBParser
 from Bio.PDB import parse_pdb_header
+
 # from Bio.PDB import PDBIO
 
 # from Bio.SeqUtils import seq1
@@ -37,20 +38,24 @@ def get_alpha_carbons(
     protein = pdb_parser.get_structure(id=pdb_id, file=filename)
     protein_atoms = protein.get_atoms()
     alpha_carbon_coords = []
-    if use_chains:
+    if (
+        use_chains
+    ):  # reading a solved structure file where only certain chains are wanted
         [
             alpha_carbon_coords.append(atom.coord)
             for atom in protein_atoms
-            if atom.name == "CA" and atom.element == "C" and atom.get_full_id()[2] in chains
+            if atom.name == "CA"
+            and atom.element == "C"
+            and atom.get_full_id()[2] in chains
         ]
-    else:
+    else:  # reading a structure prediction file where all chains are wanted
         [
             alpha_carbon_coords.append(atom.coord)
             for atom in protein_atoms
             if atom.name == "CA" and atom.element == "C"
         ]
     header = parse_pdb_header(filename)
-    if header['has_missing_residues']:
+    if header["has_missing_residues"]:
         alpha_carbon_coords = [-1]
     return alpha_carbon_coords
     # pdb_io.set_structure(protein)
